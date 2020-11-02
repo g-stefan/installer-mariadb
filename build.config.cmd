2fd34defa66ddfcec2e3b1de187c096faf0afaea
Grigore Stefan <g_stefan@yahoo.com>
2020-11-02 02:30:16 +0200

    update

--- build.config.cmd
+++ build.config.cmd
@@ -3,4 +3,6 @@ rem Public domain
 rem http://unlicense.org/
 rem Created by Grigore Stefan <g_stefan@yahoo.com>
 
+set PRODUCT_NAME=installer-mariadb
 set PRODUCT_VERSION=10.5.6
+set PRODUCT_BASE=mariadb
