b84bd38502d2e870b72936fb2d7df4ddbf609e0e
Grigore Stefan <g_stefan@yahoo.com>
2021-03-21 23:36:23 +0200

    update

--- build.config.cmd
+++ build.config.cmd
@@ -4,5 +4,5 @@ rem http://unlicense.org/
 rem Created by Grigore Stefan <g_stefan@yahoo.com>
 
 set PRODUCT_NAME=installer-mariadb
-set PRODUCT_VERSION=10.5.8
+set PRODUCT_VERSION=10.5.9
 set PRODUCT_BASE=mariadb
